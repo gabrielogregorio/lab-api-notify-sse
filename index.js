6871f5f540fd83626fa42c75b868d2229f1181bc
Gabriel Gregorio <gabriel.gregorio.1@outlook.com>
2023-10-29 09:13:12 -0300

    fix: port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const sse = require("sse-express");
 
 const app = express();
-const port = 3333;
+const port = 10000;
 
 app.get("/listen", sse, (req, res) => {
   const user = req.header("X-User");
